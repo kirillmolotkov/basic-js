c1115a064b2f3e6191d2f449ef029f79538bfdcd
BalbesGoda <balbes_goda@mail.ru>
2022-05-01 21:48:22 +0300

    done carbon-dating, count-cats, dream-team

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,24 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function dateSample(sampleActivity) {
+  if(typeof sampleActivity !== 'string'){
+    return false;
+}
+  let numberSampleActivity = Number(sampleActivity);
+  const K = Math.log(2) / HALF_LIFE_PERIOD;
+  let result;
+  if(typeof numberSampleActivity === 'number'){
+    result = Math.ceil(Math.log(MODERN_ACTIVITY/numberSampleActivity)/K);
+  }else if(typeof numberSampleActivity === 'NaN'){
+      return NaN;
+  }
+  if(result > 0 && result < Infinity){
+      return result;
+  }else if(result === Infinity){
+      return false;
+  }
+  return false;
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function createDreamTeam(members) {
+  if(Array.isArray(members)){
+    let nameTeam = [];
+    for(let elem of members){
+      if(typeof elem === 'string'){
+          for(let i = 0; i < elem.length; i++){
+            if(elem[i] !== ' '){
+              nameTeam.push(elem[i].toUpperCase())
+              break;
+            }
+          }
+      }
+     }
+     return nameTeam.sort().join('')
+  }
+  return false;
 }
 
 module.exports = {

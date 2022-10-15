b3b74ca98575a139fe57bda8e9c08986bbfb55e7
ururu <lowlevelskill@mail.ru>
2022-10-15 13:44:47 +0300

    feat: add common-character-count and delete-digit solution

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,11 +11,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(string_1, string_2) {
+  const array_1 = string_1.split("");
+  const array_2 = string_2.split("");
+  let result = 0;
+  for (let i = 0; i < array_1.length; i++) {
+    for (let j = 0; j < array_2.length; j++) {
+      if (array_1[i] === array_2[j]) {
+        array_2.splice(j, 1);
+        array_1.splice(i, 1);
+
+        i = 0;
+        j = 0;
+        result++;
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const arrayStringNumber = String(n).split("");
+  let copyArrayStringNumber = arrayStringNumber.slice();
+  const arrayResultValue = [];
+
+  for (let i = 0; i < arrayStringNumber.length; i++) {
+    copyArrayStringNumber = arrayStringNumber.slice();
+    copyArrayStringNumber.splice(i, 1);
+    arrayResultValue.push(Number(copyArrayStringNumber.join("")));
+  }
+  return Math.max(...arrayResultValue);
 }
 
 module.exports = {
-  deleteDigit
+  deleteDigit,
 };
